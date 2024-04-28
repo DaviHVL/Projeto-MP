d1c2c2d395412ff4e33da0ee7cfbb84aa4b2696b
Davi <davihvl@gmail.com>
2024-04-28 09:13:42 -0300

    Teste 4

--- romanos.cpp
+++ romanos.cpp
@@ -1,7 +1,11 @@
 #include "romanos.hpp"
 #include <unordered_map>
 
-//implementação da função para verificar se há apenas algarismos romanos
+/*
+ Implementação da função para verificar
+ se há apenas algarismos romanos.
+*/
+
 bool verificador_caracteres(const string& num_romano) {
     const string letras = "IVXLCDM";
     for (char caracter : num_romano) {
@@ -12,80 +16,103 @@ bool verificador_caracteres(const string& num_romano) {
     return true;
 }
 
-//implementação da função para verificar se as letras: V, L e D aparecem duas vezes seguidas
-bool verificador_vld(const string& num_romano){
-    if (num_romano.length() == 1){
+/*
+Implementação da função para verificar
+se as letras: V, L e D aparecem duas vezes seguidas.
+*/
+
+bool verificador_vld(const string& num_romano) {
+    if (num_romano.length() == 1) {
         return true;
     }
 
-    if (num_romano.length() == 2  && num_romano[0] == num_romano[1] && num_romano[0] == 'V')  {
+    if (num_romano.length() == 2  &&
+         num_romano[0] == num_romano[1] &&
+          num_romano[0] == 'V')  {
             return false;
     }
 
-    if (num_romano.length() == 2  && num_romano[0] == num_romano[1] && num_romano[0] == 'L')  {
+    if (num_romano.length() == 2  &&
+         num_romano[0] == num_romano[1] &&
+          num_romano[0] == 'L')  {
             return false;
     }
 
-    if (num_romano.length() == 2  && num_romano[0] == num_romano[1] && num_romano[0] == 'D')  {
+    if (num_romano.length() == 2  &&
+         num_romano[0] == num_romano[1] &&
+          num_romano[0] == 'D')  {
             return false;
     }
-    for (size_t i =0; i<(num_romano.length()-2); i++) {
-        if (num_romano[i] == num_romano[i+1] && num_romano[i] == 'V'){
+    for (size_t i =0; i < (num_romano.length()-2); i++) {
+        if (num_romano[i] == num_romano[i+1] &&
+             num_romano[i] == 'V') {
             return false;
         }
-        if (num_romano[i] == num_romano[i+1] && num_romano[i] == 'L'){
+        if (num_romano[i] == num_romano[i+1] &&
+             num_romano[i] == 'L') {
             return false;
         }
-        if (num_romano[i] == num_romano[i+1] && num_romano[i] == 'D'){
+        if (num_romano[i] == num_romano[i+1] &&
+             num_romano[i] == 'D') {
             return false;
         }
     }
     return true;
 }
 
-//implementação da função para verificar se uma letra aparece mais de três vezes seguidas
-bool verificador_mais(const string& num_romano){
+/*
+Implementação da função para verificar
+se uma letra aparece mais de três vezes seguidas.
+*/
+
+bool verificador_mais(const string& num_romano) {
     if (num_romano.length() < 4) {
             return true;
     }
-    for (size_t i =0; i<=(num_romano.length()-4); i++) {
-        if (num_romano[i] == num_romano[i+1] && num_romano[i+1] == num_romano[i+2] && num_romano[i+2] == num_romano[i+3]) {
+    for (size_t i =0; i <= (num_romano.length()-4); i++) {
+        if (num_romano[i] == num_romano[i+1] &&
+             num_romano[i+1] == num_romano[i+2] &&
+              num_romano[i+2] == num_romano[i+3]) {
             return false;
         }
     }
     return true;
 }
 
-//função principal
-int romanos_para_decimal(const string& num_romano){
-    if (!verificador_caracteres(num_romano)){
+/*
+Função principal.
+*/
+
+int romanos_para_decimal(const string& num_romano) {
+    if (!verificador_caracteres(num_romano)) {
         return -1;
     }
-    if (!verificador_vld(num_romano)){
+    if (!verificador_vld(num_romano)) {
         return -1;
     }
-    if (!verificador_mais(num_romano)){
+    if (!verificador_mais(num_romano)) {
         return -1;
     }
 
-    unordered_map<char,int>mapa_romano = {
-        {'I',1}, {'V',5}, {'X',10}, {'L',50},
-        {'C',100}, {'D',500}, {'M',1000}
+    unordered_map<char, int>mapa_romano = {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}
     };
 
-    int result=0;
+    int result = 0;
 
-    for (size_t i =0; i<(num_romano.length()); i++) {
-        if (i+1 < num_romano.length() && mapa_romano[num_romano[i+1]] > mapa_romano[num_romano[i]]){
-            result-=mapa_romano[num_romano[i]];
-        } else{
-            result+=mapa_romano[num_romano[i]];
+    for (size_t i = 0; i < (num_romano.length()); i++) {
+        if (i+1 < num_romano.length() &&
+             mapa_romano[num_romano[i+1]] > mapa_romano[num_romano[i]]) {
+            result -= mapa_romano[num_romano[i]];
+        } else {
+            result += mapa_romano[num_romano[i]];
         }
     }
 
-    if (result<0) {
+    if (result < 0) {
         return -1;
-    } else{
+    } else {
         return result;
     }
 }
--- romanos.hpp
+++ romanos.hpp
@@ -3,16 +3,25 @@
 
 using namespace std;
 
-//função para verificar se há apenas algarismos romanos
+/*
+Função para verificar se há apenas algarismos romanos.
+*/
 bool verificador_caracteres(const string& num_romano);
 
-//As letras: V, L e D não podem aparecer duas vezes seguidas
-//função para verificar se há isso ocorre ou não
+/*
+As letras: V, L e D não podem aparecer duas vezes seguidas.
+Função para verificar se há isso ocorre ou não.
+*/
 bool verificador_vld(const string& num_romano);
 
-//Uma letra não pode aparecer mais de três vezes seguidas
-//função para verificar se há isso ocorre ou não
+/*
+Uma letra não pode aparecer mais de três vezes seguidas.
+Função para verificar se há isso ocorre ou não.
+*/
 bool verificador_mais(const string& num_romano);
 
-//função principal
+/*
+Função principal.
+*/
 int romanos_para_decimal(const string& num_romano);
+
